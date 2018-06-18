d3c2cf8bb248d03128207dff9ad4fc456d230569
Sibusiso Ngqondo <sngqondo@hotmail.com>
2018-06-18 15:29:27 +0200

    Queue test

--- app/src/main/java/za/co/absa/teamcitytest/LoginActivity.java
+++ app/src/main/java/za/co/absa/teamcitytest/LoginActivity.java
@@ -92,13 +92,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
     }
 
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
         }
-
         getLoaderManager().initLoader(0, null, this);
     }
 
